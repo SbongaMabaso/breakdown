7d0cfb8c0c61355746fcf664dc7fea64ac35c5a8
Sbonga Mabaso <mabasos@wavesoft.co.za>
2023-03-02 16:25:30 +0200

    adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>BreakDown Mob</h1>
+      <h1 className="text-3xl font-bold underline">
+        BreakDown Mob
+      </h1>
     </div>
   );
 }
