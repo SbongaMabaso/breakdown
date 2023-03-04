584d4b46fe21283e0d9e3326718b6b582ac47eb7
Sbonga Mabaso <mabasos@wavesoft.co.za>
2023-03-04 19:58:56 +0200

    adding modal, controller and connection string

---
+++ breakedownApi/breakedownApi/Breakdown.cs
@@ -0,0 +1,11 @@
+﻿namespace breakedownApi
+{
+    public class Breakdown
+    {
+        public string BreakdownRef { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public string DriverName { get; set; } = string.Empty;
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public DateTime BreakdownDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ breakedownApi/breakedownApi/Controllers/BreakdownController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace breakedownApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakdownController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BreakdownController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
