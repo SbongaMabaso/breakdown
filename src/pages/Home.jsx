bfe194022f9a07cbd2fd92dbb004e2cfd06de12a
Sbonga Mabaso <mabasos@wavesoft.co.za>
2023-03-04 03:29:07 +0200

    bottom border on header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="flex flex-col items-center h-screen bg-indigo-400">
-        <div className="w-screen p-9 bg-indigo-600 rounded-md">
+        <div className="border-b-4 w-screen p-9 bg-indigo-600 rounded-md">
             <h3>Break<span>Down</span></h3>
         </div>
 
